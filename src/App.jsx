9570a4d4ce8f10c5584ae4f26ec069293781e447
czimbalmoskristof23 <czimbalmoskristof23@Kalmar-16>
2025-02-21 17:53:44 +0100

    main2

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
 
       <p>main1</p>
+      <p>main2</p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
